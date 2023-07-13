fdd3b52d6ac7e873db89ba253ff1f0fb050ccac0
Georgia <georgia.m.vrana@gmail.com>
2023-07-13 14:26:26 -0400

    uploading to the cloud for help

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+
+function move(event) {
+
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+   
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width++;
+        elem.style.width = width + '%';
+      }
+    }
+
+}
+
+move(event)
