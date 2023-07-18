1d25504708efe8000534d20c9d07ac5830c92d38
Georgia <georgia.m.vrana@gmail.com>
2023-07-18 18:05:07 -0400

    adding values to the slider thumb

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const slider = document.getElementById("myRange");
 const output = document.getElementById("demo");
 output.innerHTML = slider.value; // Display the default slider value
+const thumbWidth = 25; // adjust this to the actual width of your thumb
 
-// Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = this.value;
   let percentage = (this.value - this.min) / (this.max - this.min) * 100;
   this.style.background = `linear-gradient(to right, #d3d3d3 ${percentage}%, #400A14 ${percentage}%)`;
+  output.style.left = `calc(${percentage}% - (25px / 2))`;
+  output.innerHTML = `%${slider.value}<`;
 }
+
+slider.addEventListener('input', slider.oninput);
 
 slider.oninput();
 
